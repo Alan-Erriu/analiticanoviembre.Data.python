b22eb2e23720b1f303e7cd6800ccca83c81131dc
Alan Erriu <erriu0273@gmail.com>
2024-06-21 08:39:53 -0300

    validacion de mails agregado

---
+++ ejercicios/clase4AlanErriu/Servicios/validacionesServicio.py
@@ -0,0 +1,12 @@
+import re
+
+
+
+# ['test@example.com', 'hello@world.com.ar']
+# Validar un formato de dirección de correo electrónico
+
+def validarMail(mail:str)->bool: 
+  patron=r'\b[A-Za-z0-9._%+-]+@[A-za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+  listMailsEncontrados = re.findall(patron,mail) 
+  if len(listMailsEncontrados) == 0: return False
+  return True
--- ejercicios/clase4AlanErriu/main.py
+++ ejercicios/clase4AlanErriu/main.py
@@ -1,22 +1,25 @@
-#Siguiente Dia: Definir una función que reciba un parametro de fecha y devuelva la siguiente fecha calendario a la recibida
+
 #Validar un formato de dirección de correo electrónico
 
 #Definir una funcion que reciba 3 parametros (archivoOrigen, archivoDestino y palabra) y filtre y elimine del archivo la palabra que entra por parametro
-from Servicios.fechaServicio import obtenerDiaSiguiente, fechaValida
+from Servicios import  fechaServicio, validacionesServicio
 
-fechaSiguiente = obtenerDiaSiguiente("1/2/2025")
+#Siguiente Dia: Definir una función que reciba un parametro de fecha y devuelva la siguiente fecha calendario a la recibida
+fechaSiguiente = fechaServicio.obtenerDiaSiguiente("1/2/2025")
 print(fechaSiguiente)
-
-
 
 ## para testear a groso modo que funciona 
 fechaInicial = "31/12/2024"
 for x in range(1,400):
-    mañana = obtenerDiaSiguiente(fechaInicial)
+    mañana = fechaServicio.obtenerDiaSiguiente(fechaInicial)
     print(mañana)
-    pasadoMañana = obtenerDiaSiguiente(mañana)
+    pasadoMañana = fechaServicio.obtenerDiaSiguiente(mañana)
     print(pasadoMañana)
     fechaInicial = pasadoMañana
+
+emailEjemplo = 'test@example.com'
+emailEsVerdadero =  validacionesServicio.validarMail(emailEjemplo)
+print(emailEsVerdadero)
 
 
 
